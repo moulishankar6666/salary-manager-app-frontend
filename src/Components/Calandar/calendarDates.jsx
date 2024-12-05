66528a6770589493dd81bc5d14a8813605e46922
moulishankar6666 <moulishankar3669@gmail.com>
2024-12-05 16:28:36 +0530

    working on calendar section Dates feature

--- src/Components/Calandar/calendarDates.jsx
+++ src/Components/Calandar/calendarDates.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+
 const CalendarDates = () => {
-  const date = new Date().toString().split(" ");
-  //   alert(date);
+  const [activeDate, setActiveDate] = useState(
+    new Date().toString().split(" ")
+  );
+  const Today = new Date();
+  const today = new Date(Today.getFullYear(), Today.getMonth(), activeDate[2]);
+  const arr = [
+    -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4,
+    5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+  ];
 
   const eachItem = (i) => {
+    const date = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + i
+    )
+      .toString()
+      .split(" ");
+
     const day = date[2][0] === "0" ? date[2][1] : date[2];
-    // alert(date);
+
+    const isactive = date[2] === activeDate[2] ? "True" : null;
+
     return (
-      <li>
+      <li
+        key={i}
+        onClick={() => setActiveDate(date)}
+        className={`${isactive ? "active" : "in-active"}`}
+      >
         <p className="month">{date[1]}</p>
         <p className="date">{day}</p>
         <p className="day">{date[0]}</p>
@@ -14,13 +37,15 @@ const CalendarDates = () => {
     );
   };
 
-  const allDates = () => {
-    for (let i = 0; i < 30; i++) {
-      eachItem(i);
-    }
-  };
-
-  return <ul className="calendar-dates-main-container">{eachItem()}</ul>;
+  return (
+    <div>
+      <ul className="calendar-dates-main-container">
+        {arr.map((eachDate) => {
+          return eachItem(eachDate);
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default CalendarDates;
